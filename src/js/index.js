e922e178b167eda222bfa391e932ca3f64c892cf
Sofia <sofiaxrk@gmail.com>
2022-01-31 14:07:31 +0000

    componentes creados y css

--- src/js/index.js
+++ src/js/index.js
@@ -3,10 +3,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 // include your styles into the webpack bundle
-import "../styles/index.css";
+import "../styles/main.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+import Main from "../../src/view/main.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<Main />, document.querySelector("#app"));
